289fe71bcfd995fb686243c682923758c4392620
kHitoshi <kubotah1115@gmail.com>
2023-12-29 20:04:42 +0900

    Modified to allow multiple selection and editing of transform components
    This reverts commit 98259c49b97da4c2f249920ef8476a9ee9ceaa8c.

--- Primal/Editor/Util/Controls/NumberBox.cs
+++ Primal/Editor/Util/Controls/NumberBox.cs
@@ -61,6 +61,7 @@ namespace Editor.Util.Controls
             e.Handled = true;
 
             _mouseXStart = e.GetPosition(this).X;
+            Focus();
         }
 
         private void OnTextBlock_Mouse_LBU(object sender, MouseButtonEventArgs e)

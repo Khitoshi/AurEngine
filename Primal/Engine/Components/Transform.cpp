592e6e0334c9a0f6b4d38caa286e35666b8958cd
kHitoshi <kubotah1115@gmail.com>
2024-01-17 22:10:49 +0900

    Creating of script components.

--- Primal/Engine/Components/Transform.cpp
+++ Primal/Engine/Components/Transform.cpp
@@ -11,7 +11,7 @@ namespace aur::transform
 		utl::vector<math::v3> scales;
 	}
 
-	component create_transform(const init_info& info, game_entity::entity entity)
+	component create(init_info info, game_entity::entity entity)
 	{
 		assert(entity.is_valid());
 		const id::id_type entity_index{ id::index(entity.get_id()) };
@@ -33,7 +33,7 @@ namespace aur::transform
 		return component(transform_id{ (id::id_type)positions.size() - 1 });
 	}
 
-	void remove_transform(component c)
+	void remove(component c)
 	{
 		assert(c.is_valid());
 	}

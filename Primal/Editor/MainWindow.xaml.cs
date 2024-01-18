2663ad08610bdcee7a6dd84efe554ff6125ba1fe
kHitoshi <kubotah1115@gmail.com>
2024-01-18 22:08:46 +0900

    Add process to place visualstudio solution in project data when creating a new project Create process to have it populated before creating a project if no environment variables are set

--- Primal/Editor/MainWindow.xaml.cs
+++ Primal/Editor/MainWindow.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using Editor.GameProject;
 using Editor.Pages;
 using System.ComponentModel;
+using System.IO;
 using System.Text;
 using System.Windows;
 using System.Windows.Controls;
@@ -10,7 +11,6 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace Editor
 {
@@ -19,15 +19,19 @@ namespace Editor
     /// </summary>
     public partial class MainWindow : Window
     {
+        public static string PrimalPath { get; private set; } = @"D:\Study\Engine\AurEngine\Primal";
+
         public MainWindow()
         {
             InitializeComponent();
-            this.Loaded += OnMainWindowLoaded;
+            Loaded += OnMainWindowLoaded;
+            Closing += OnMainWindowClosing;
         }
 
         private void OnMainWindowLoaded(object sender, RoutedEventArgs e)
         {
-            this.Loaded -= OnMainWindowLoaded;
+            Loaded -= OnMainWindowLoaded;
+            GetEnginePath();
             OpenProjectBrowserDialog();
         }
 
@@ -49,7 +53,30 @@ namespace Editor
         private void OnMainWindowClosing(object sender, CancelEventArgs e)
         {
             Closing -= OnMainWindowClosing;
+
             Project.Current?.Unload();
+        }
+
+        private void GetEnginePath()
+        {
+            var primalPath = Environment.GetEnvironmentVariable("AUR_ENGINE", EnvironmentVariableTarget.User);
+            if (primalPath == null || !Directory.Exists(Path.Combine(primalPath, @"Engine\EngineAPI")))
+            {
+                var dlg = new EnginePathDialog();
+                if (dlg.ShowDialog() == true)
+                {
+                    PrimalPath = dlg.PrimalPath;
+                    Environment.SetEnvironmentVariable("AUR_ENGINE", PrimalPath.ToUpper(), EnvironmentVariableTarget.User);
+                }
+                else
+                {
+                    Application.Current.Shutdown();
+                }
+            }
+            else
+            {
+                PrimalPath = primalPath;
+            }
         }
 
     }
